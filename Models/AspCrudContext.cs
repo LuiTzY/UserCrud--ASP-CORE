8a35dbdf18e7cd77f1bab3be2e2e7869e71e0e5c
Luis Marte <luisdavidmarte00@gmail.com>
2024-10-05 17:11:42 -0400

    First Commit

---
+++ Models/AspCrudContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAsp.Models;
+
+public partial class AspCrudContext : DbContext
+{
+    public AspCrudContext()
+    {
+    }
+
+    public AspCrudContext(DbContextOptions<AspCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3213E83F9ABF6638");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Password)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
